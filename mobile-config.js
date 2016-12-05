67a25d654e5be2ea77668df7aeb1cef40ab42c39
Daniel <danieldelgadilloh@gmail.com>
2016-12-05 00:49:04 -0500

    cambios

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.grupoddv.industec.monteza',
+  name: 'Industec',
+  description: 'APP',
+  author: 'INDUSTEC - DDV',
+  email: 'danieldelgadilloh@gmail.com',
+  website: 'http://industec.grupoddv.com'
+});
